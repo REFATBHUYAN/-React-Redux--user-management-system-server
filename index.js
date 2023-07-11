f8c50b8f3f2631f3f55987fe493e816d5dcdc0d7
REFATBHUYAN <refatbubt@gmail.com>
2023-07-12 03:07:58 +0600

    finished

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dp83dff.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,6 +45,29 @@ async function run() {
         }
   
         const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+
+      app.put("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedClass = req.body;
+        const updateDoc = {
+          $set: {
+            name: updatedClass.name,
+            email: updatedClass.email,
+            phone: updatedClass.phone,
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+  
+        res.send(result);
+      });
+
+      app.delete("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
         res.send(result);
       });
 
